cd143718ab6a1d7c11c4e8d6ee6a7a21cd74d5a3
Samodya <samodya0203samadini@gmail.com>
2025-05-31 16:32:23 +0530

    Add files via upload

---
+++ Day20/Codes/recievef.c
@@ -0,0 +1,33 @@
+
+//Reciever.c
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+//structure for message queue
+struct mesg_buffer{
+	long mesg_type;
+	char mesg_text[100];
+}message;
+int main(){
+key_t key;
+int msgid;
+
+//ftok to generate unique key
+key = ftok("progfile119",119);
+
+//msg creates message queue
+//and return identifier
+
+msgid = msgget(key,0666|IPC_CREAT);
+
+//msgrcv to recieve message
+msgrcv(msgid,&message, sizeof(message), 1, 0);
+//display the message
+printf("Data recieved is: %s \n",message.mesg_text);
+//to destroy the message queue
+msgctl(msgid, IPC_RMID, NULL);
+
+return 0;
+
+}
